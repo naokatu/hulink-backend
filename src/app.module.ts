290752526be376f016b6687b18faaa45bda03dad
naokatu <naokatu4@gmail.com>
2024-03-24 23:12:39 +0900

    feat: config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import configuration from './config/configuration'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+      ignoreEnvFile: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/configuration.ts
@@ -0,0 +1,53 @@
+/**
+ * 環境変数の取得を行う
+ * @param key
+ */
+function getEnv(key: string): string {
+  const value = process.env[key]
+
+  if (!value) {
+    throw new Error(`environment variable ${key} is not defined`)
+  }
+
+  return value
+}
+
+// 文字列を boolean に変換する
+function parseBoolean(value: string): boolean {
+  if (value === 'true') {
+    return true
+  }
+
+  if (value === 'false') {
+    return false
+  }
+
+  throw new Error(`environment variable is not boolean: ${value}`)
+}
+
+// 文字列を number に変換する
+function parseNumber(value: string): number {
+  const parsed = Number(value)
+
+  if (isNaN(parsed)) {
+    throw new Error(`environment variable is not number: ${value}`)
+  }
+
+  return parsed
+}
+
+export type Config = {
+  APP_ENV: string
+  LOG_PRETTY_PRINT: boolean
+  LOG_LEVEL: string
+  DO_AUTHENTICATION: boolean
+}
+
+export default (): Config => {
+  return {
+    APP_ENV: getEnv('APP_ENV'),
+    LOG_PRETTY_PRINT: parseBoolean(getEnv('LOG_PRETTY_PRINT')),
+    LOG_LEVEL: getEnv('LOG_LEVEL'),
+    DO_AUTHENTICATION: parseBoolean(getEnv('DO_AUTHENTICATION')),
+  }
+}
