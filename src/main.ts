5e71581250b3ebafede906d20622955168b3ed81
naokatu <naokatu4@gmail.com>
2024-03-27 18:15:59 +0900

    feat: enable exception filter

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,15 @@ import { VersioningType } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 
 import { AppModule } from './app.module'
+import { HttpExceptionFilter } from './filters/http-exception.filter'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableVersioning({
     type: VersioningType.URI,
-    defaultVersion: '1'
+    defaultVersion: '1',
   })
+  app.useGlobalFilters(new HttpExceptionFilter())
   await app.listen(3000)
 }
 bootstrap()
