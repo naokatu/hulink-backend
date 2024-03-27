607f2e27b0e96aca259e93da98ad88e049f4969e
naokatu <naokatu4@gmail.com>
2024-03-27 15:42:28 +0900

    feat: test seeder

---
+++ src/database/seeders/test/link-user.ts
@@ -0,0 +1,37 @@
+import { PrismaClient } from '@prisma/client'
+import * as db from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export const userEmmaId = 'e247b44c-cd10-7df8-92a7-7184e0c657d1'
+export const userLilyId = 'd08510a5-83e2-1ab4-0f1d-a6e3f937a059'
+export const userSamId = 'cf581cb2-267e-b094-ba0d-ba790aa14308'
+
+export async function createLinkUser() {
+  const createManyInput: db.Prisma.LinkUserCreateManyInput[] = [
+    {
+      id: userEmmaId,
+      name: 'Emma',
+      encount: 10,
+      label: 'family',
+      sex: 'female',
+    },
+    {
+      id: userLilyId,
+      name: 'Lily',
+      encount: 10,
+      label: 'family',
+      sex: 'female',
+    },
+    {
+      id: userSamId,
+      name: 'Sam',
+      encount: 1,
+      label: 'friend',
+      sex: 'male',
+    },
+  ]
+  await prisma.linkUser.createMany({
+    data: createManyInput,
+  })
+}
---
+++ src/database/seeders/test/user.ts
@@ -0,0 +1,28 @@
+import { PrismaClient } from '@prisma/client'
+import * as db from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export const userJohnId = 'e6e9bfb8-eedb-91a1-265c-46217b54fd5a'
+export const userMikeId = '598915c4-c4fb-2a17-fc10-01cc4830d6f5'
+export const userEmilyId = '124a57ed-4b15-d61b-ebcc-4067ac83191a'
+
+export async function createUser() {
+  const createManyInput: db.Prisma.UserCreateManyInput[] = [
+    {
+      id: userJohnId,
+      name: 'john',
+    },
+    {
+      id: userMikeId,
+      name: 'Mike',
+    },
+    {
+      id: userEmilyId,
+      name: 'Emily',
+    },
+  ]
+  await prisma.user.createMany({
+    data: createManyInput,
+  })
+}
