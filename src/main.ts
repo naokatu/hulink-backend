ee92ae67bed2b8287e81dc9b3c8887348b2b1aed
naokatu <naokatu4@gmail.com>
2024-03-27 12:13:58 +0900

    fix: eslint

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
+import { VersioningType } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 
 import { AppModule } from './app.module'
-import { VersioningType } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
