1a6960225f830cc00ab60d4762391a3ffdc83cce
naokatu <naokatu4@gmail.com>
2024-04-05 01:47:13 +0900

    feat: model

---
+++ src/model/claim.ts
@@ -0,0 +1,4 @@
+export interface Claim {
+  uid: string
+  email: string
+}
---
+++ src/model/user.ts
@@ -0,0 +1,10 @@
+export interface User {
+  id: string
+  firebaseUid: string
+  email: string
+  name: string
+  createdUserId: string
+  updatedUserId: string
+  createdAt: Date
+  updatedAt: Date
+}
