d86dd0abf36ceb507858001931068824fc0ef335
naokatu <naokatu4@gmail.com>
2024-05-05 17:02:31 +0900

    feat: usecase

--- src/usecase/link-user/link-user.usecase.ts
+++ src/usecase/link-user/link-user.usecase.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common'
 
 import { PrismaService } from '../../prisma.service'
 import { LinkUserRepository } from '../../repository/link-user/link-user.repository'
+import { uuidv7 } from '../../utils/generate-uuid'
 import * as usecase from './link-user.interface.usecase'
 
 @Injectable()
@@ -17,6 +18,30 @@ export class LinkUserUsecase {
 
       return {
         linkUsers,
+      }
+    })
+  }
+
+  async createLinkUser(
+    input: usecase.CreateLinkUserInput,
+  ): Promise<usecase.CreateLinkUserOutput> {
+    return this.prisma.$transaction(async (tx) => {
+      const linkUserId = uuidv7()
+
+      const createdLinkUser = await this.linkUserRepository.createLinkUser(tx, {
+        id: linkUserId,
+        name: input.name,
+        createdUserId: input.user.id,
+        updatedUserId: input.user.id,
+        user: {
+          connect: {
+            id: input.user.id,
+          },
+        },
+      })
+
+      return {
+        linkUser: createdLinkUser,
       }
     })
   }
